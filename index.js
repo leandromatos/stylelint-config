b356f1816d1d8af184adaffe7d9e53cf80b5297a
Leandro Matos <contato@leandromatos.com.br>
2020-10-15 11:00:44 -0300

    Update rules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     'prettier/prettier': true,
     'color-hex-length': 'long',
-    'declaration-property-value-blacklist': {
+    'declaration-property-value-disallowed-list': {
       '/^border/': [''],
     },
     'font-family-name-quotes': 'always-where-recommended',
